3c9ae6d2c5a3938efacf6523d48c38741178bcc0
webanantgupta <anantguptastu0698@gmail.com>
2026-09-04 11:21:05 +0530

    Update Postgres supabase

--- config/db.js
+++ config/db.js
@@ -1,12 +1,36 @@
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const db = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+// db.connect()
+//   .then(() => {
+//     console.log("✅ PostgreSQL Connected");
+//   })
+//   .catch((err) => {
+//     console.error("❌ Database connection failed:", err);
+//   });
+
+// module.exports = db;
+
+
+
+
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect()
@@ -18,8 +42,3 @@ db.connect()
   });
 
 module.exports = db;
-
-
-
-
-
